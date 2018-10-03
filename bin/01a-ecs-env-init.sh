65337ff7a90257bc354d4e500ab82343567d3dd1
Dan McGuane <daniel.mcguane@evertrue.com>
2018-10-03 12:42:38 -0400

    allow usage of stage/prod or staging/production

--- bin/01a-ecs-env-init.sh
+++ bin/01a-ecs-env-init.sh
@@ -6,14 +6,14 @@ echo "Starting ecs-env-init"
 if [[ "$APP_ENV" == "" ]]; then
 	# Extract env with https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-introspection.html
 	hostip=$(ip route show | awk '/default/ {print $3}')
-	APP_ENV=$(curl -s http://${hostip}:51678/v1/metadata | jq -r '.Cluster' | grep -oE 'stage|prod')
+	APP_ENV=$(curl -s http://${hostip}:51678/v1/metadata | jq -r '.Cluster' | grep -oE 'stage|staging|prod|production')
 
 	echo 'ECS host metadata'
 	curl -s http://$hostip:51678/v1/metadata
 	echo ""
 
 	# If the cluster info cannot be found then default to stage
-	if [[ "$APP_ENV" == "" ]] || [[ "$APP_ENV" == "dev" ]]; then
+	if [[ "$APP_ENV" == "" ]] || [[ "$APP_ENV" == "dev" ]] || [[ "$APP_ENV" == "development" ]]; then
 		echo "Host environment not found - setting to stage"
 
 		APP_ENV=stage
@@ -22,13 +22,13 @@ else
 	echo "APP_ENV value set at runtime to "$APP_ENV
 fi
 
-# If the PASSENGER_APP_ENV value is not set then default to staging.  
+# If the PASSENGER_APP_ENV value is not set then default to staging.
 # This passenger docker setting is required to properly route traffic to the Ruby application.
 if [[ "$PASSENGER_APP_ENV" == "" ]]; then
 	echo "Setting PASSENGER_APP_ENV value, using APP_ENV "$APP_ENV" to set value"
 
-	if [[ "$APP_ENV" == "prod" ]]; then
-		PASSENGER_APP_ENV="production"	
+	if [[ "$APP_ENV" == "prod" ]] || [[ "$APP_ENV" == "production" ]]; then
+		PASSENGER_APP_ENV="production"
 	else
 		PASSENGER_APP_ENV="staging"
 	fi
